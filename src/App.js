c9223ebd025b8ab9219a31fc1247f6fd7b985a20
Praveen Kumar <3.pravin@gmail.com>
2018-11-04 21:21:49 +0000

    sample issue
    Signed-off-by: Praveen Kumar <3.pravin@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Radio from '@govuk-react/radio';
+import MultiChoice from '@govuk-react/multi-choice';
+import * as Header from '@govuk-react/header'
+import ErrorSummary from '@govuk-react/error-summary';
+import InputField from '@govuk-react/input-field';
 
 class App extends Component {
   render() {
+    const description = "Some description";
+    const errors = [
+      {
+        targetName: 'national-insurance-number',
+        text: 'National Insurance number error',
+      }
+    ];
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+        <ErrorSummary 
+          heading={'Some header'}
+          description={description}
+          errors={errors}
+        />
+        <InputField
+          name="national-insurance-number"
+          hint="It’s on your National Insurance card, benefit letter, payslip or P60."
+        >
+        National Insurance number
+        </InputField>
       </div>
     );
   }
